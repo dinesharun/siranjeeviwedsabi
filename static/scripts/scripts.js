4660f3279201a0f8a359ad5d712d43e73ad121c6
dinesharun <dnsh.arun@gmail.com>
2017-09-19 16:38:53 +0200

    Add first animations

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,1 +1,46 @@
+currpgid = 100;
+
+function navTo(entry, pgid) {
+  if(entry == 0) {
+    if(currpgid != 100) {
+      switch(currpgid) {
+        case 0:
+          $("#groom").removeClass('groomintro');
+          $("#bride").removeClass('brideintro');
+          $("#homeinvite").removeClass('inviintro');
+          $("#groom").addClass('groomexit');
+          $("#bride").addClass('brideexit');
+          $("#homeinvite").addClass('inviexit');
+          break;
+          
+        default:
+          $("#placeholder").removeClass('plhintro');
+          $("#placeholder").addClass('plhexit');
+          break;
+      }
+      
+      setTimeout(function () { navTo(1, pgid); }, 1800);
+    } else {
+      setTimeout(function () { navTo(1, pgid); }, 10);
+    }
+  } else {
+    switch(pgid) {
+      case 0:
+        $("#groom").removeClass('groomexit');
+        $("#bride").removeClass('brideexit');
+        $("#homeinvite").removeClass('inviexit');
+        $("#groom").addClass('groomintro');
+        $("#bride").addClass('brideintro');
+        $("#homeinvite").addClass('inviintro');
+        break;
+        
+      default:
+        $("#placeholder").removeClass('plhexit');
+        $("#placeholder").addClass('plhintro');
+        break;
+    }
+    
+    currpgid = pgid;
+  }
+}
 
